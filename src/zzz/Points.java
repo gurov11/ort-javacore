274367d04bcadc54e222aced8685c9f88032011c
Паша <@192.168.0.100>
2016-11-14 21:21:00 +0200

    задача1

---
+++ src/zzz/Points.java
@@ -0,0 +1,21 @@
+package zzz;
+
+public class Points {
+private int x,y;
+private String name;
+Points(int a ,int b,String r){
+	x = a;
+	y = b;
+	name = r;
+}
+public int getX(){
+	return x;
+}
+public int getY(){
+	return y;
+}
+
+public String getName(){
+	return name;
+}
+}
